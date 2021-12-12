5dbea2b85e26889292cf5d1f797ec643e742a308
virpil <firstvirpil@gmail.com>
2021-12-12 23:32:41 +0700

    Initial commit

---
+++ firefoxdriver/main.py
@@ -0,0 +1,26 @@
+import time
+
+from selenium import webdriver
+
+
+url = "https://www.vk.com/"
+driver = webdriver.Firefox(executable_path="geckodriver.exe")
+
+try:
+    driver.get(url=url)
+    driver.save_screenshot("vk.png")
+    time.sleep(5)
+    # driver.get("https://stackoverflow.com/")
+    # time.sleep(5)
+
+    # driver.refresh()
+    # driver.get_screenshot_as_file("1.png")
+    # driver.get(url="https://stackoverflow.com/")
+    # time.sleep(5)
+    # driver.save_screenshot("2.png")
+    # time.sleep(2)
+except Exception as ex:
+    print(ex)
+finally:
+    driver.close()
+    driver.quit()
