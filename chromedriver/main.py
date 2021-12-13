68ce59698f0875903b27bd686d371a1fdd33e926
virpil <firstvirpil@gmail.com>
2021-12-13 15:11:21 +0700

    2

--- chromedriver/main.py
+++ chromedriver/main.py
@@ -1,14 +1,38 @@
+# from selenium import webdriver
+from seleniumwire import webdriver
 import time
+import random
+from fake_useragent import UserAgent
 
-from selenium import webdriver
+# url = "https://www.instagram.com/"
+# url_whatis = "https://www.whatismybrowser.com/detect/what-is-my-user-agent"
+url_2ip = "https://2ip.ru"
 
+user_agent_list = [
+    "best_of_the_best",
+    "life_is_life",
+    "oo_java_agent"
+]
 
-url = "https://www.instagram.com/"
-driver = webdriver.Chrome(executable_path="chromedriver.exe")
+# change useragent
+useragent = UserAgent()
 
+# options
+options = webdriver.ChromeOptions()
+
+# выбор агента произвольный из списка
+# options.add_argument(f"user-agent={random.choice(user_agent_list)}")
+
+options.add_argument(f"user-agent={useragent.random}")
+
+# set proxy
+options.add_argument("--proxy-server=138.128.91.65:8000")
+
+# driver = webdriver.Chrome(executable_path="chromedriver", options=options)
+driver = webdriver.Chrome(executable_path="chromedriver", options=options)
 try:
-    driver.get(url=url)
-    time.sleep(5)
+    # driver.get(url=url_whatis)
+    # time.sleep(5)
     # driver.get("https://stackoverflow.com/")
     # time.sleep(5)
 
@@ -18,6 +42,8 @@ try:
     # time.sleep(5)
     # driver.save_screenshot("2.png")
     # time.sleep(2)
+    driver.get(url=url_2ip)
+    time.sleep(5)
 except Exception as ex:
     print(ex)
 finally:
--- firefoxdriver/main.py
+++ firefoxdriver/main.py
@@ -1,13 +1,21 @@
 import time
-
 from selenium import webdriver
+from fake_useragent import UserAgent
 
+# url = "https://www.vk.com/"
+url_whatis = "https://www.whatismybrowser.com/detect/what-is-my-user-agent"
 
-url = "https://www.vk.com/"
-driver = webdriver.Firefox(executable_path="geckodriver.exe")
+useragent = UserAgent()
+# options
+options = webdriver.FirefoxOptions()
 
+# change useragent
+# options.set_preference("general-useragent.override", "Hello Friends!")
+options.set_preference("general-useragent.override", useragent.random)
+
+driver = webdriver.Firefox(executable_path="geckodriver", options=options)
 try:
-    driver.get(url=url)
+    driver.get(url=url_whatis)
     driver.save_screenshot("vk.png")
     time.sleep(5)
     # driver.get("https://stackoverflow.com/")
